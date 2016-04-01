d44483ab2f07c3f97d2be01aee4b34f76c59a60d
Tim Chan <tim@chan.net>
2016-04-01 16:50:42 -0700

    init commit

---
+++ src/main/scala/com/example/ExampleServer.scala
@@ -0,0 +1,22 @@
+package com.example
+
+import com.twitter.finagle.http.{Response, Request}
+import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.http.routing.HttpRouter
+
+object ExampleServerMain extends ExampleServer
+
+class ExampleServer extends HttpServer {
+
+  override def defaultFinatraHttpPort = ":9999"
+
+  override def configureHttp(router: HttpRouter) {
+    router
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[CommonFilters]
+      .add[PingController]
+  }
+
+}
