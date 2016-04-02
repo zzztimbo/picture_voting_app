54fbfff86c7fe1eb790fd214ea59f4cf7ae2b490
Tim Chan <tim@chan.net>
2016-04-02 07:18:59 -0700

    forgot to add TextHandler

--- src/main/scala/co/adhoclabs/handlers/TextHandler.scala
+++ src/main/scala/co/adhoclabs/handlers/TextHandler.scala
@@ -1,8 +1,18 @@
 package co.adhoclabs.handlers
 
-/**
-  * Created by tim on 4/2/2016.
-  */
-class TextHandler {
+import co.adhoclabs.stateful.PictureVoteCounter
 
+class TextHandler(val pictureVote: String) extends PictureSetHandler {
+
+  def isValidPicture(picture: String) = getPicturesSet().contains(picture)
+
+  def vote(): Unit = {
+    if (isValidPicture(pictureVote)) {
+      PictureVoteCounter.vote(pictureVote)
+    }
+  }
+}
+
+object TextHandler {
+  def apply(pictureVote: String) = new TextHandler(pictureVote).vote()
 }
